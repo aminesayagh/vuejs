213f42b4714a21b2b268e33a148bf5199c60ddd2
aminesayagh <aminesayagh1997@gmail.com>
2020-11-14 21:24:54 +0100

    edite 1

---
+++ vuejs/src/components/HomeLink.vue
@@ -0,0 +1,28 @@
+// ./components/homeLink.vue
+<template>
+    <div id="app">
+		<nav>
+			<NavLink url="/" text="Accueil" />
+			<NavLink url="/about" text="À propos" />
+			<NavLink url="/contact" text="Contact" />
+		</nav>
+	</div>
+</template>
+
+<script>
+import navLink from './components/navLink.vue'
+
+export default {
+    name: 'HomeLink',
+    components: {
+        navLink
+    }
+}
+</script>
+
+<style>
+    a {
+        font-weight: 800;
+    }
+
+</style>
